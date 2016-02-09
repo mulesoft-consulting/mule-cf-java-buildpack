74cfbddf09ce4592b55ecd06aa0c153ec8accedd
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2016-02-09 15:42:51 -0500

    Added some logging

--- lib/java_buildpack/container/mule.rb
+++ lib/java_buildpack/container/mule.rb
@@ -121,8 +121,8 @@ module JavaBuildpack
           return
         end
 
-        @logger.info { "AMC Setup Script Exists? " + File.exist?("#{@droplet.sandbox}/bin/amc_setup") }
-        @logger.info { "JAVA_HOME Exists? " + File.exist?("#{@droplet.java_home.root}/bin/java") }
+        @logger.info { "AMC Setup Script Exists? " + File.exist?("#{@droplet.sandbox}/bin/amc_setup").to_s }
+        @logger.info { "JAVA_HOME Exists? " + File.exist?("#{@droplet.java_home.root}/bin/java") }.to_s
 
         cmd = [
             "JAVA_HOME=#{@droplet.java_home.root}",
